6494c978c329b500da318d425cc6acc5219851ed
Jimmbo19 <jega190112@gmail.com>
2024-09-06 03:15:03 +0000

    final

--- src/js/component/listado.jsx
+++ src/js/component/listado.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const Listado = () => {
     const [items, setItems] = useState([]);
     const [inputValue, setInputValue] = useState('');
@@ -24,23 +25,29 @@ const Listado = () => {
         
 
             <div className=" container text-center margin=3">
-                <h1>TODOS</h1>
+                <h1 className="display-2 lead">TODOS</h1>
                 <div>
                     <input 
-                        type="text"  
+                        type="text"  className="form-control list-inline"
                         onKeyDown={addItem} 
                         onChange={e => setInputValue(e.target.value)} 
                         value={inputValue} 
+                        placeholder="What needs to be done?"
                     />
                 </div>
                 <ul className="list-group">
                     {items.map((item, index) => (
-                        <li  className="list-group-item shadow p-3  bg-body-tertiary rounded" key={index} >
-                            {item}<button type="button" className="btn-close d-flex align-items-start" aria-label="Close" onClick={()=>removeitem(index)} ></button>
+                        <li  className="list-group-item shadow p-3  bg-body-tertiary rounded d-flex justify-content-between" key={index}  >
+                            {item}<button type="button" className="btn-close " aria-label="Close" onClick={()=>removeitem(index)}  ></button>
                         
                         </li>
+                  
                     ))}
+                       <li className="list-footer-item shadow p-1  bg-body-tertiary rounded d-flex justify-content-between">
+                    <span>{items.length} items left</span>
+                   </li>
                 </ul>
+
             </div>
 
         
